acbd5cfba94329ff8d3f38777cafc8f86e80189c
Evan Andreolli <Evan Andreolli>
2016-08-27 08:52:28 -0500

    wtf

---
+++ assets/javascript/app.js
@@ -0,0 +1,64 @@
+var questions = ["John S-117 is the only Spartan-II", "The Halo: CE Magnum is a three shot kill", "Midship is a multiplayer map on Halo 2", "Halo 3 was the first Halo to introduce the Battle Rifle", "The Flood just want to give you a hug"];
+var answers = ["True", "False"];
+var time = 30;
+var correct = 0;
+var incorrect = 0;
+var unanswered = 0;
+
+
+
+
+$ (document).ready(function() {
+
+	//make the start screen button
+
+	function startbutton() {
+		//$(".form").html("<button> Start! </button>");
+		var start = "<button class= 'starter'>Begin!</button>";
+
+		$(".container").html(start);
+
+
+
+	}
+
+
+
+
+
+
+
+
+
+
+	//then generate question page plus timer countdown plus end button
+
+
+
+
+
+
+
+
+
+	//on end button or end of countdown show stats screen
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
